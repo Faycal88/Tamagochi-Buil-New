b59d83cb03f2ff700237508b0bdf6ff4ca1294bd
Faycal <merabet.faycal@outlook.com>
2023-10-11 10:56:50 +0100

    hot fix

---
+++ main.js
@@ -0,0 +1,20 @@
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+
+
+const FirebaseConfig = {
+    apiKey: "AIzaSyA2VH9s-D3FUrhhxPER1tmzCnYYZL83gLo",
+    authDomain: "unity-ff7cc.firebaseapp.com",
+    databaseURL: "https://unity-ff7cc-default-rtdb.firebaseio.com",
+    projectId: "unity-ff7cc",
+    storageBucket: "unity-ff7cc.appspot.com",
+    messagingSenderId: "177895607834",
+    appId: "1:177895607834:web:da017a0139fde1c29a4ba3",
+    measurementId: "G-7P93VSKKXL"
+  };
+
+
+const app = initializeApp(FirebaseConfig);
+const auth = getAuth(app);
+
+
